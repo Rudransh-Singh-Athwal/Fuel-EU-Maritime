09c59fa16fefcd668f38f8ca6119f991447c7e49
Rudransh Singh Athwal <rsathwal04@gmail.com>
2026-03-03 00:34:30 +0530

    chore: add prisma schema, migrations, and database seed script

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,5 +1,13 @@
-module.exports = {
-  migrate: {
-    url: process.env.DATABASE_URL,
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+    seed: "ts-node prisma/seed.ts",
   },
-};
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
---
+++ backend/prisma/seed.ts
@@ -0,0 +1,74 @@
+import "dotenv/config";
+import { prisma } from "../src/infrastructure/db/prisma";
+
+async function main() {
+  await prisma.route.deleteMany();
+
+  await prisma.route.createMany({
+    data: [
+      {
+        route_id: "R001",
+        vesselType: "Container",
+        fuelType: "HFO",
+        year: 2024,
+        ghg_intensity: 91.0,
+        fuelConsumption: 5000,
+        distance: 12000,
+        totalEmissions: 4500,
+        is_baseline: true,
+      },
+      {
+        route_id: "R002",
+        vesselType: "BulkCarrier",
+        fuelType: "LNG",
+        year: 2024,
+        ghg_intensity: 88.0,
+        fuelConsumption: 4800,
+        distance: 11500,
+        totalEmissions: 4200,
+        is_baseline: false,
+      },
+      {
+        route_id: "R003",
+        vesselType: "Tanker",
+        fuelType: "MGO",
+        year: 2024,
+        ghg_intensity: 93.5,
+        fuelConsumption: 5100,
+        distance: 12500,
+        totalEmissions: 4700,
+        is_baseline: false,
+      },
+      {
+        route_id: "R004",
+        vesselType: "RoRo",
+        fuelType: "HFO",
+        year: 2025,
+        ghg_intensity: 89.2,
+        fuelConsumption: 4900,
+        distance: 11800,
+        totalEmissions: 4300,
+        is_baseline: false,
+      },
+      {
+        route_id: "R005",
+        vesselType: "Container",
+        fuelType: "LNG",
+        year: 2025,
+        ghg_intensity: 90.5,
+        fuelConsumption: 4950,
+        distance: 11900,
+        totalEmissions: 4400,
+        is_baseline: false,
+      },
+    ],
+  });
+}
+
+main()
+  .catch((e) => {
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
