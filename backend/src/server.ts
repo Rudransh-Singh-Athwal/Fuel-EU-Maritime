0e8d69393e95bacf8935115574187c29c75a197f
Rudransh Singh Athwal <rsathwal04@gmail.com>
2026-03-03 12:07:03 +0530

    prepared backend for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,17 @@ import { createRouter } from "./adapters/inbound/http/routes";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://fuel-eu-maritime-rsa.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 const routeRepo = new PrismaRouteRepository();
